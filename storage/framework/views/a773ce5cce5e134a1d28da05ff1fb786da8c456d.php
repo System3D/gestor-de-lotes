1fae6f642cb1d35cd67ca80e10bca7bd17740f26
Luciano Tonet <tonetlds@gmail.com>
2016-01-27 08:25:32 -0200

    Adicionado icone

--- storage/framework/views/a773ce5cce5e134a1d28da05ff1fb786da8c456d.php
+++
@@ -1,7 +0,0 @@
-<div class="modal fade" id="modal">
-    <div class="modal-dialog">
-    	<div class="modal-content">
-      		<?php echo $__env->make('templates.modal_content', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-		</div>
-    </div>
-</div>
